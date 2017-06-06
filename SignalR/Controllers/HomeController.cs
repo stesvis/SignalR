8377d5f37dd658cfcac335429798a2c03208d5a2
Cristian Merli <cristian@thirdstream.ca>
2017-06-06 10:49:00 -0600

    first commit

---
+++ SignalR/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System.Web.Mvc;
+
+namespace SignalR.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Task()
+        {
+            for (int i = 1; i <= 30; i++)
+            {
+                System.Threading.Thread.Sleep(1000);
+            }
+            return View();
+        }
+    }
+}
